e89d944d972abb712e069bda01fdf8676d50a913
Eric Lam <tsukiko852@gmail.com>
2025-02-27 22:35:29 +0800

    disable timeout of inference server

--- mcp_bridge/openai_clients/genericHttpxClient.py
+++ mcp_bridge/openai_clients/genericHttpxClient.py
@@ -4,5 +4,5 @@ from mcp_bridge.config import config
 client: AsyncClient = AsyncClient(
     base_url=config.inference_server.base_url,
     headers={"Authorization": f"Bearer {config.inference_server.api_key}", "Content-Type": "application/json"},
-    timeout=10000,
+    timeout=None,
 )
